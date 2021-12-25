46dc4870548e47856d791b5c7b1b7325ee047255
Admin <8948484+xcess_1_0@user.noreply.gitee.com>
2021-12-25 15:27:53 +0800

    init

---
+++ conf/jd.go
@@ -0,0 +1,6 @@
+package conf
+
+const (
+	AppSecret = ""
+	JdAppKey  = ""
+)
--- jduion/jd.union.open.promotion.tools.intelligence.query.go
+++ jduion/jd.union.open.promotion.tools.intelligence.query.go
@@ -1,5 +1,7 @@
 package jduion
 
+import "encoding/json"
+
 type JdUnionOpenPromotionToolsIntelligenceQueryResponse struct {
 	JdUnionOpenPromotionToolsIntelligenceQueryResponse struct {
 		QueryResult struct {
@@ -18,4 +20,38 @@ type JdUnionOpenPromotionToolsIntelligenceQueryResponse struct {
 			Message string `json:"message"`
 		} `json:"queryResult"`
 	} `json:"jd_union_open_promotion_tools_intelligence_query_response"`
+}
+
+type JdUnionOpenPromotionToolsIntelligenceQueryRequest struct {
+	Req struct {
+		Title      string `json:"title"`
+		Type       int    `json:"type"`
+		Cid1List   []int  `json:"cid_1_list"`
+		Status     int    `json:"status"`
+		Essence    string `json:"essence"`
+		PageIndex  int    `json:"pageIndex"`
+		PageSize   int    `json:"pageSize"`
+		UnionId    int    `json:"unionid"`
+		Pid        string `json:"pid"`
+		SubUnionId string `json:"subUnionId"`
+		SiteId     int    `json:"siteId"`
+		PositionId int    `json:"positionId"`
+	} `json:"req"`
+}
+
+func (J *JdSdk) JdUnionOpenPromotionToolsIntelligenceQuery(query interface{}) (result *JdUnionOpenPromotionToolsIntelligenceQueryResponse) {
+	J.SysParams.Method = "jd.union.open.category.goods.get"
+	bodyBytes := J.BodyBytes(query)
+	//result := &CategoryResponse{}
+	//e := json.Unmarshal([]byte(bodyBytes), &result)
+	//if e != nil {
+	//	panic(e)
+	//}
+	//categoryResult = &CateGoryResult{}
+	e := json.Unmarshal(bodyBytes, &result)
+	if e != nil {
+		panic(e)
+	}
+	return result
+
 }
